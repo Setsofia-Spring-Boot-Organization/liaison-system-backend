607e32fed8f9264e562aa925a5890c611a16e5a9
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-09-03 08:48:34 +0000

    Project setup

---
+++ src/main/java/com/backend/liaison_system/LiaisonSystemApplication.java
@@ -0,0 +1,13 @@
+package com.backend.liaison_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaisonSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaisonSystemApplication.class, args);
+	}
+
+}
