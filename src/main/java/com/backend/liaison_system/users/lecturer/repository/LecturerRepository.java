eba826c66315e65e20bd234c300f705729c19c96
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-09-15 11:48:41 +0000

    Code update: get other lecturers that belong to the same department as the current lecturer

--- src/main/java/com/backend/liaison_system/users/lecturer/repository/LecturerRepository.java
+++ src/main/java/com/backend/liaison_system/users/lecturer/repository/LecturerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -29,4 +30,6 @@ public interface LecturerRepository extends JpaRepository<Lecturer, String> {
      */
     @NonNull
     Page<Lecturer> findAll(@NonNull Pageable pageable);
+
+    List<Lecturer> findAllByDepartment(String department);
 }
