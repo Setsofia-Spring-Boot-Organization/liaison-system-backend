95089fef06fc2b9c67403fc34a52c8d00c023feb
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-09-16 09:56:16 +0000

    Code update: a new feature (zone) was added in order to help users create new zones

---
+++ src/main/java/com/backend/liaison_system/zone/entity/Zone.java
@@ -0,0 +1,42 @@
+package com.backend.liaison_system.zone.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Zone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String region;
+    private String zoneLead;
+
+    @Embedded
+    private ZoneLecturers lecturers;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Zone zone = (Zone) o;
+        return getId() != null && Objects.equals(getId(), zone.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
