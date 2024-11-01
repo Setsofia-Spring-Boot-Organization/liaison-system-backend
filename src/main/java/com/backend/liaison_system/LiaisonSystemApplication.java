82670e6151855dce60395ab33bcd7e9317e70a22
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-01 12:24:58 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/LiaisonSystemApplication.java
+++ src/main/java/com/backend/liaison_system/LiaisonSystemApplication.java
@@ -2,10 +2,12 @@ package com.backend.liaison_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
 @SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement
 public class LiaisonSystemApplication {
 
 	public static void main(String[] args) {
