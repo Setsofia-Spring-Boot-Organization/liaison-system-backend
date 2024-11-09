29889b31e34f544f8f2cde7196666d3c7eef6738
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-09 12:10:25 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/zone/service/ZoneService.java
+++ src/main/java/com/backend/liaison_system/zone/service/ZoneService.java
@@ -1,5 +1,6 @@
 package com.backend.liaison_system.zone.service;
 
+import com.backend.liaison_system.common.ConstantRequestParam;
 import com.backend.liaison_system.dao.Response;
 import com.backend.liaison_system.zone.dto.NewZone;
 import com.backend.liaison_system.zone.entity.Zone;
@@ -17,7 +18,7 @@ public interface ZoneService {
      * @param zones the NewZone object containing the details of the new zone to be created
      * @return a ResponseEntity containing a Response object with the status of the zone creation operation
      */
-    ResponseEntity<Response<?>> createNewZone(String id, List<NewZone> zones, boolean internship);
+    ResponseEntity<Response<?>> createNewZone(String id, List<NewZone> zones, ConstantRequestParam param);
 
-    ResponseEntity<Response<Zone>> getAllZones(String adminId, boolean internship);
+    ResponseEntity<Response<List<Zone>>> getAllZones(String adminId, ConstantRequestParam param);
 }
