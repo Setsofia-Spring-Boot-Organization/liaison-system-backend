9917929eb8ed8074aaf307fae08dd1d6140d5ac6
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-09 09:54:11 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/zone/service/ZoneService.java
+++ src/main/java/com/backend/liaison_system/zone/service/ZoneService.java
@@ -2,6 +2,7 @@ package com.backend.liaison_system.zone.service;
 
 import com.backend.liaison_system.dao.Response;
 import com.backend.liaison_system.zone.dto.NewZone;
+import com.backend.liaison_system.zone.entity.Zone;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -13,8 +14,10 @@ public interface ZoneService {
      * It validates the admin's authorization before proceeding with the zone creation.
      *
      * @param id the ID of the admin user making the request
-     * @param zone the NewZone object containing the details of the new zone to be created
+     * @param zones the NewZone object containing the details of the new zone to be created
      * @return a ResponseEntity containing a Response object with the status of the zone creation operation
      */
     ResponseEntity<Response<?>> createNewZone(String id, List<NewZone> zones);
+
+    ResponseEntity<Response<Zone>> getAllZones(String adminId, boolean internship);
 }
