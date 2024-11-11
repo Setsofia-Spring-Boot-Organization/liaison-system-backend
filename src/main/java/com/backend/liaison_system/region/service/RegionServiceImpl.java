50b1ee4c82b458edbbfe0c66126b84d75655658a
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-11 12:48:31 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/region/service/RegionServiceImpl.java
+++ src/main/java/com/backend/liaison_system/region/service/RegionServiceImpl.java
@@ -5,6 +5,7 @@ import com.backend.liaison_system.exception.Error;
 import com.backend.liaison_system.exception.LiaisonException;
 import com.backend.liaison_system.region.dto.NewRegion;
 import com.backend.liaison_system.region.entities.Region;
+import com.backend.liaison_system.region.entities.Town;
 import com.backend.liaison_system.region.repository.RegionRepository;
 import com.backend.liaison_system.users.admin.util.AdminUtil;
 import jakarta.transaction.Transactional;
@@ -26,41 +27,18 @@ public class RegionServiceImpl implements RegionService {
 
     @Override
     @Transactional(rollbackOn = {RuntimeException.class, LiaisonException.class})
-    public ResponseEntity<Response<?>> creatNewRegion(String id, List<NewRegion> newRegionsRequest) {
+    public ResponseEntity<Response<?>> creatNewRegion(String id, List<NewRegion> newRegions) {
         adminUtil.verifyUserIsAdmin(id);
 
         // get all the regions
         List<Region> existingRegions = (List<Region>) regionRepository.findAll();
 
-        List<Region> newRegions = new ArrayList<>();
-        for (NewRegion newRegion : newRegionsRequest) {
-            for (Region region : existingRegions) {
-
-                // when a regions already exists, update the towns in it
-                if (region.getRegion().equalsIgnoreCase(newRegion.region())) {
-
-                    newRegion.towns().removeAll(region.getTown().towns());
-                    region.getTown().towns().addAll(newRegion.towns());
-                    region.setDateUpdated(LocalDateTime.now());
-
-                    newRegions.add(region);
-                } else {
-
-                    // create a new region
-                    Region newRegion1 = new Region();
-
-                    newRegion1.setRegion(newRegion.region());
-                    newRegion1.setDateCreated(LocalDateTime.now());
-                    newRegion1.setDateUpdated(LocalDateTime.now());
-                    newRegion1.setTown(region.getTown());
-
-                    newRegions.add(newRegion1);
-                }
-            }
-        }
+        List<Region> regions = (existingRegions.isEmpty()) ?
+                createNewRegion(newRegions) :
+                updateExistingRegion(newRegions, existingRegions);
 
         try {
-            List<Region> savedRegions = (List<Region>) regionRepository.saveAll(newRegions);
+            List<Region> savedRegions = (List<Region>) regionRepository.saveAll(regions);
 
             Response<List<Region>> response = Response.<List<Region>>builder()
                     .status(HttpStatus.CREATED.value())
@@ -69,8 +47,43 @@ public class RegionServiceImpl implements RegionService {
                     .build();
 
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
         } catch (Exception e) {
             throw new LiaisonException(Error.ERROR_SAVING_DATA);
         }
+    }
+
+    private List<Region> createNewRegion(List<NewRegion> newRegions) {
+
+        List<Region> regions = new ArrayList<>();
+        for (NewRegion newRegion : newRegions) {
+            Region reg = new Region();
+
+            reg.setRegion(newRegion.region());
+            reg.setDateCreated(LocalDateTime.now());
+            reg.setDateUpdated(LocalDateTime.now());
+            reg.setTown(new Town(newRegion.towns()));
+
+            regions.add(reg);
+        }
+
+        return regions;
+    }
+
+    private List<Region> updateExistingRegion(List<NewRegion> newRegions, List<Region> existingRegions) {
+
+        List<Region> regions = new ArrayList<>();
+
+        for (NewRegion newRegion : newRegions) {
+            for (Region existingRegion : existingRegions) {
+                if (newRegion.region().equals(existingRegion.getRegion())) {
+                    existingRegion.getTown().towns().addAll(newRegion.towns());
+
+                    regions.add(existingRegion);
+                }
+            }
+        }
+
+        return regions;
     }
 }
