374ba685503c480b2cff051e1c1cda6d8209b17d
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-11 11:41:22 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/zone/controller/ZoneController.java
+++ src/main/java/com/backend/liaison_system/zone/controller/ZoneController.java
@@ -3,8 +3,8 @@ package com.backend.liaison_system.zone.controller;
 import com.backend.liaison_system.common.ConstantRequestParam;
 import com.backend.liaison_system.dao.Response;
 import com.backend.liaison_system.exception.LiaisonException;
+import com.backend.liaison_system.zone.dao.AllZones;
 import com.backend.liaison_system.zone.dto.NewZone;
-import com.backend.liaison_system.zone.entity.Zone;
 import com.backend.liaison_system.zone.service.ZoneService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -29,7 +29,7 @@ public class ZoneController {
     }
 
     @GetMapping(path = "/{admin-id}")
-    public ResponseEntity<Response<List<Zone>>> getAllZones(
+    public ResponseEntity<Response<List<AllZones>>> getAllZones(
             @PathVariable("admin-id") String adminId,
             ConstantRequestParam param
     ) {
