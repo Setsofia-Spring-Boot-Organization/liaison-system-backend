cdfef5ac0f26321ab2cbeb5ac164f8439d2a47d0
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-11 11:41:05 +0000

    Code update:

---
+++ src/main/java/com/backend/liaison_system/region/entities/Region.java
@@ -0,0 +1,26 @@
+package com.backend.liaison_system.region.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+
+    private LocalDateTime dateCreated;
+    private LocalDateTime dateUpdated;
+
+    @Embedded
+    private Town town;
+}
