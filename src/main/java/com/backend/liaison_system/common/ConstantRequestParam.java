e1360d887ae33d24004d1c1d228a39d36bdc7724
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-13 15:07:18 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/common/ConstantRequestParam.java
+++ src/main/java/com/backend/liaison_system/common/ConstantRequestParam.java
@@ -3,5 +3,17 @@ package com.backend.liaison_system.common;
 public record ConstantRequestParam(
     String startOfAcademicYear,
     String endOfAcademicYear,
-    boolean internship
-) { }
+    boolean internship,
+    int page,
+    int size
+) {
+    @Override
+    public int page() {
+        return 1;
+    }
+
+    @Override
+    public int size() {
+        return 10;
+    }
+}
