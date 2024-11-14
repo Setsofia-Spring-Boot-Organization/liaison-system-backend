5c681634985eeab0c30bd43a4219333741f3bb79
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-14 08:24:03 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/exception/ExceptionResponse.java
+++ src/main/java/com/backend/liaison_system/exception/ExceptionResponse.java
@@ -1,15 +1,7 @@
 package com.backend.liaison_system.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionResponse {
 
@@ -17,4 +9,45 @@ public class ExceptionResponse {
     private String error;
     private String message;
     private String path;
+
+    public ExceptionResponse(int status, String error, String message, String path) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public ExceptionResponse() {}
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
