aa915ed3f07e311e5492274124586bbd0d57c9ab
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-14 08:22:44 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/dao/Response.java
+++ src/main/java/com/backend/liaison_system/dao/Response.java
@@ -1,18 +1,82 @@
 package com.backend.liaison_system.dao;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@Builder
-@ToString
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response<T> {
     private int status;
     private String message;
     private T data;
+
+    public Response(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Response() {}
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+    private Response (Builder<T> builder) {
+        this.status = builder.status;
+        this.message = builder.message;
+        this.data = builder.data;
+    }
+
+    public static class Builder<T> {
+        private int status;
+        private String message;
+        private T data;
+
+        public Builder<T> status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder<T> message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder<T> data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public Response<T> build() {
+            return new Response<>(this);
+        }
+    }
 }
