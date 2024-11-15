1586c7f540e137e7f2bb91bd989641faca7b8d4a
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-15 01:35:17 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/users/admin/dashboard/DashboardController.java
+++ src/main/java/com/backend/liaison_system/users/admin/dashboard/DashboardController.java
@@ -1,10 +1,9 @@
 package com.backend.liaison_system.users.admin.dashboard;
 
+import com.backend.liaison_system.common.ConstantRequestParam;
 import com.backend.liaison_system.dao.Response;
-import com.backend.liaison_system.dto.ConstantRequestParam;
 import com.backend.liaison_system.exception.LiaisonException;
 import com.backend.liaison_system.users.admin.dashboard.dao.Statistics;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +19,8 @@ public class DashboardController {
     @GetMapping(path = "/{admin-id}")
     public ResponseEntity<Response<Statistics>> getStatistics(
             @PathVariable("admin-id") String id,
-            ConstantRequestParam constantRequestParam
+            ConstantRequestParam param
     ) throws LiaisonException {
-        return dashboardService.getStatistics(id, constantRequestParam);
+        return dashboardService.getStatistics(id, param);
     }
 }
