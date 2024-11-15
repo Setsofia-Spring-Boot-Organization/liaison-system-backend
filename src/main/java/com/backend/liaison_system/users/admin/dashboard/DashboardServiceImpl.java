a074584739523d89a414674511bccb054b10a9e7
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-15 01:35:43 +0000

    Code update:

--- src/main/java/com/backend/liaison_system/users/admin/dashboard/DashboardServiceImpl.java
+++ src/main/java/com/backend/liaison_system/users/admin/dashboard/DashboardServiceImpl.java
@@ -1,7 +1,7 @@
 package com.backend.liaison_system.users.admin.dashboard;
 
+import com.backend.liaison_system.common.ConstantRequestParam;
 import com.backend.liaison_system.dao.Response;
-import com.backend.liaison_system.dto.ConstantRequestParam;
 import com.backend.liaison_system.enums.InternshipType;
 import com.backend.liaison_system.exception.Error;
 import com.backend.liaison_system.exception.LiaisonException;
@@ -12,7 +12,6 @@ import com.backend.liaison_system.users.lecturer.entity.Lecturer;
 import com.backend.liaison_system.users.lecturer.repository.LecturerRepository;
 import com.backend.liaison_system.users.student.Student;
 import com.backend.liaison_system.users.student.StudentRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -33,13 +32,13 @@ public class DashboardServiceImpl implements DashboardService{
     }
 
     @Override
-    public ResponseEntity<Response<Statistics>> getStatistics(String id, ConstantRequestParam constantRequestParam) {
+    public ResponseEntity<Response<Statistics>> getStatistics(String id, ConstantRequestParam param) {
 
         // verify user role - ADMIN
         adminUtil.verifyUserIsAdmin(id);
 
-        List<Student> students = getTotalStudents(constantRequestParam);
-        List<Lecturer> lecturers = getTotalLecturers(constantRequestParam);
+        List<Student> students = studentRepository.findAllStudents(param);
+        List<Lecturer> lecturers = lecturerRepository.findAllLectures(param);
 
         Response<Statistics> response = new Response.Builder<Statistics>()
                 .status(HttpStatus.OK.value())
@@ -51,55 +50,5 @@ public class DashboardServiceImpl implements DashboardService{
                 )).build();
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
-
-    /**
-     * This method retrieves a list of students based on the internship type and year range.
-     * It throws a {@link LiaisonException} if the internship type is invalid.
-     *
-     * @param constantRequestParam an object containing request parameters such as internship type, start year, and end year
-     * @return a list of students that match the internship type and year range
-     * @throws LiaisonException if the internship type is invalid
-     */
-    private List<Student> getTotalStudents(ConstantRequestParam constantRequestParam) throws LiaisonException {
-
-        if (constantRequestParam.internshipType().equals(InternshipType.INTERNSHIP.name())) {
-            return studentRepository.findAllStudents(
-                    constantRequestParam.startYear(),
-                    constantRequestParam.endYear(),
-                    constantRequestParam.internshipType().toUpperCase()
-            );
-        } else if (constantRequestParam.internshipType().equals(InternshipType.SEMESTER_OUT.name())) {
-            return studentRepository.findAllStudents(
-                    constantRequestParam.startYear(),
-                    constantRequestParam.endYear(),
-                    constantRequestParam.internshipType().toUpperCase()
-            );
-        } else
-            throw new LiaisonException(Error.INVALID_INTERNSHIP_TYPE, new Throwable(Message.THE_INTERNSHIP_TYPE_IS_INCORRECT.label));
-    }
-
-    /**
-     * This method retrieves a list of lecturers based on the internship type and year range.
-     * It throws a {@link LiaisonException} if the internship type is invalid.
-     *
-     * @param constantRequestParam an object containing request parameters such as internship type, start year, and end year
-     * @return a list of lecturers that match the internship type and year range
-     * @throws LiaisonException if the internship type is invalid
-     */
-    private List<Lecturer> getTotalLecturers(ConstantRequestParam constantRequestParam) throws LiaisonException {
-
-        if (constantRequestParam.internshipType().equals(InternshipType.INTERNSHIP.name())) {
-            return lecturerRepository.findAllLectures(
-                    constantRequestParam.startYear(),
-                    constantRequestParam.endYear()
-            );
-        } else if (constantRequestParam.internshipType().equals(InternshipType.SEMESTER_OUT.name())) {
-            return lecturerRepository.findAllLectures(
-                    constantRequestParam.startYear(),
-                    constantRequestParam.endYear()
-            );
-        } else
-            throw new LiaisonException(Error.INVALID_INTERNSHIP_TYPE, new Throwable(Message.THE_INTERNSHIP_TYPE_IS_INCORRECT.label));
     }
 }
