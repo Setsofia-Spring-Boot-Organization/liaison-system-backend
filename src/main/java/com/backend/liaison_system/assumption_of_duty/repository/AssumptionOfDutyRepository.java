aec7036d043a168b3c3348227e7be1b3612cea00
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-21 09:14:15 +0000

    Code update:

---
+++ src/main/java/com/backend/liaison_system/assumption_of_duty/repository/AssumptionOfDutyRepository.java
@@ -0,0 +1,9 @@
+package com.backend.liaison_system.assumption_of_duty.repository;
+
+import com.backend.liaison_system.assumption_of_duty.entities.AssumptionOfDuty;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AssumptionOfDutyRepository extends CrudRepository<AssumptionOfDuty, String>, JpaSpecificationExecutor<AssumptionOfDuty> {
+
+}
