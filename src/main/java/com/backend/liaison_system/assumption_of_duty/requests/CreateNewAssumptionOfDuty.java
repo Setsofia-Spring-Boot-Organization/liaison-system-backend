f2292005926c759298af3c7b787e85541aa346a6
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-11-21 09:14:03 +0000

    Code update:

---
+++ src/main/java/com/backend/liaison_system/assumption_of_duty/requests/CreateNewAssumptionOfDuty.java
@@ -0,0 +1,21 @@
+package com.backend.liaison_system.assumption_of_duty.requests;
+
+public record CreateNewAssumptionOfDuty(
+        String companyName,
+        String companyPhone,
+
+        String companyExactLocation,
+        String companyTown,
+        String companyRegion,
+
+        String companyAddress,
+        String companyEmail,
+
+        String companySupervisor,
+        String supervisorPhone,
+
+        String letterTo,
+
+        String companyLongitude,
+        String companyLatitude
+) { }
