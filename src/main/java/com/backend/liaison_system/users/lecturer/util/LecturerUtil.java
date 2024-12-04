5d288c7a007e9ab35ce98516933317b47ecc20c3
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-04 13:02:46 +0000

    Code update: minor changes

---
+++ src/main/java/com/backend/liaison_system/users/lecturer/util/LecturerUtil.java
@@ -0,0 +1,31 @@
+package com.backend.liaison_system.users.lecturer.util;
+
+import com.backend.liaison_system.enums.UserRoles;
+import com.backend.liaison_system.exception.Error;
+import com.backend.liaison_system.exception.LiaisonException;
+import com.backend.liaison_system.exception.Message;
+import com.backend.liaison_system.users.lecturer.repository.LecturerRepository;
+import org.springframework.stereotype.Component;
+
+import static com.backend.liaison_system.exception.Error.USER_NOT_FOUND;
+
+@Component
+public class LecturerUtil {
+
+
+    private final LecturerRepository lecturerRepository;
+
+    public LecturerUtil(LecturerRepository lecturerRepository) {
+        this.lecturerRepository = lecturerRepository;
+    }
+
+    public void verifyUserIsStudent(String id) {
+        lecturerRepository.findById(id).ifPresentOrElse((lecturer -> {
+                    if (!lecturer.getRole().equals(UserRoles.LECTURER)) {
+                        throw new LiaisonException(Error.UNAUTHORIZED_USER, new Throwable(Message.THE_USER_IS_NOT_AUTHORIZED.label));
+                    }
+                }),
+                () -> {throw new LiaisonException(USER_NOT_FOUND, new Throwable(Message.USER_NOT_FOUND_CAUSE.label));}
+        );
+    }
+}
