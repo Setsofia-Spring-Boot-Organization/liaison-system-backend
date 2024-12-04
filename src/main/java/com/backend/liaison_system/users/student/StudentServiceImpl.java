87a7dda2b3ded7cb1e8eba2bc861ba27dd51819c
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-04 06:02:44 +0000

    Code update: minor changes

--- src/main/java/com/backend/liaison_system/users/student/StudentServiceImpl.java
+++ src/main/java/com/backend/liaison_system/users/student/StudentServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Service
@@ -35,7 +36,9 @@ public class StudentServiceImpl implements StudentService{
                         data.getStudentEmail(),
                         data.getProfilePictureUrl(),
                         data.isSupervised(),
-                        data.isAssumeDuty()
+                        data.isAssumeDuty(),
+                        //TODO: add the assigned lecturers here
+                        List.of()
                 ))
         );
 
