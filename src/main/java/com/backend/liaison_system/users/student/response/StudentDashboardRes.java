c7a08678ed4a868ef66832a55a54d23b11b2de2b
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-04 06:04:08 +0000

    Code update: minor changes

--- src/main/java/com/backend/liaison_system/users/student/response/StudentDashboardRes.java
+++ src/main/java/com/backend/liaison_system/users/student/response/StudentDashboardRes.java
@@ -1,10 +1,13 @@
 package com.backend.liaison_system.users.student.response;
 
+import java.util.List;
+
 public record StudentDashboardRes(
         String id,
         String name,
         String email,
         String profilePictureUrl,
         boolean isSupervised,
-        boolean isAssumeDuty
+        boolean isAssumeDuty,
+        List<AssignedLecturer> assignedLecturers
 ) { }
