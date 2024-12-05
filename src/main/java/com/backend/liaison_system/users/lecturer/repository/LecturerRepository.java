90f454edf22d5cfde61e5e1039b39584d311943e
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-05 10:44:43 +0000

    Code update: minor changes

--- src/main/java/com/backend/liaison_system/users/lecturer/repository/LecturerRepository.java
+++ src/main/java/com/backend/liaison_system/users/lecturer/repository/LecturerRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -82,4 +84,9 @@ public interface LecturerRepository extends JpaRepository<Lecturer, String>, Jpa
                 LecturerSpecification.getAllLecturers(param)
         );
     }
+
+    @Modifying
+    @Transactional
+    @Query(value = "DROP TABLE IF EXISTS lecturer", nativeQuery = true)
+    void dropLecturerTable();
 }
