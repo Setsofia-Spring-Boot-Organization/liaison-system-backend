ebd4620a091f6b31cc3c7f7b9f9ac0ebe1122c17
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-10 12:47:55 +0000

    Code update: minor changes

--- src/main/java/com/backend/liaison_system/users/admin/dao/StudentLocationData.java
+++ src/main/java/com/backend/liaison_system/users/admin/dao/StudentLocationData.java
@@ -3,7 +3,7 @@ package com.backend.liaison_system.users.admin.dao;
 public record StudentLocationData(
         StudentDetails studentDetails,
         CompanyDetails companyDetails,
-        int lat,
-        int lng
+        double lat,
+        double lng
 ) {
 }
