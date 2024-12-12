aabb264d9932161f5181f648ebd4e2c36cd4bd62
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-12 12:37:48 +0000

    Code update: minor changes

--- src/main/java/com/backend/liaison_system/util/UAcademicYear.java
+++ src/main/java/com/backend/liaison_system/util/UAcademicYear.java
@@ -4,19 +4,23 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Component
 public class UAcademicYear {
 
     public static LocalDateTime startOfAcademicYear(String year) {
         int startYear = Integer.parseInt(year);
-
-        return LocalDate.of(startYear, 1, 1).atStartOfDay();
+        LocalDate startOfYear = LocalDate.of(startYear, 1, 1);
+        return startOfYear.atStartOfDay(ZoneId.of("UTC")).toLocalDateTime();
     }
 
     public static LocalDateTime endOfAcademicYear(String year) {
         int endYear = Integer.parseInt(year);
-
-        return LocalDate.of(endYear, 12, 31).atTime(23, 59, 59, 999999999);
+        LocalDate endOfYear = LocalDate.of(endYear, 12, 31);
+        LocalDateTime endOfYearTime = endOfYear.atTime(23, 59, 59, 999999999);
+        ZonedDateTime zonedEndOfYearTime = endOfYearTime.atZone(ZoneId.of("UTC"));
+        return zonedEndOfYearTime.toLocalDateTime();
     }
 }
