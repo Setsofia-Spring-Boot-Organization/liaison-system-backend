d6610a6d58bc82e465bf49345d1c2a6def4d0267
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-30 21:35:54 +0000

    Code update: minor changes

--- src/main/java/com/backend/liaison_system/users/lecturer/controller/LecturerController.java
+++ src/main/java/com/backend/liaison_system/users/lecturer/controller/LecturerController.java
@@ -7,10 +7,23 @@ import com.backend.liaison_system.users.dao.LecturerList;
 import com.backend.liaison_system.users.lecturer.entity.Lecturer;
 import com.backend.liaison_system.users.lecturer.service.LecturerService;
 import com.backend.liaison_system.users.lecturer.dto.NewLecturerRequest;
+import com.backend.liaison_system.users.lecturer.util.LecturerUtil;
+import com.backend.liaison_system.users.student.Student;
+import com.backend.liaison_system.users.student.StudentRepository;
+import com.backend.liaison_system.users.student.supervision.SupervisionRepository;
+import com.backend.liaison_system.users.student.supervision.suoervision_report.SupervisionReportGenerator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.DateFormatter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping(path = "liaison/api/v1/lecturers")
@@ -50,9 +63,29 @@ public class LecturerController {
     @PutMapping(path = "/{lecturer-id}/supervise/{student-id}")
     public ResponseEntity<Response<?>> superviseStudent(
             @PathVariable("lecturer-id") String lecturerId,
-            @PathVariable("student-id") String studentId
+            @PathVariable("student-id") String studentId,
+            ConstantRequestParam param
     ) {
-        return lecturerService.superviseStudent(lecturerId, studentId);
+        return lecturerService.superviseStudent(lecturerId, studentId, param);
+    }
+
+
+    @GetMapping(path = "/{supervisor-id}/students/supervised")
+    public ResponseEntity<Response<?>> getStudentsSupervisedBySupervisor(
+            @PathVariable("supervisor-id") String supervisorId,
+            ConstantRequestParam param
+    ) {
+        return lecturerService.getStudentsSupervisedBySupervisor(supervisorId, param);
+    }
+
+
+    @GetMapping(path = "/{supervisor-id}/supervision/report")
+    public void generateStudentSupervisionReport(
+            HttpServletResponse response,
+            @PathVariable("supervisor-id") String supervisorId,
+            ConstantRequestParam param
+    ) throws IOException {
+        lecturerService.generateStudentSupervisionReport(response, supervisorId, param);
     }
 
 
