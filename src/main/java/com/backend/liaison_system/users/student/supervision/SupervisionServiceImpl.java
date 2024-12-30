a04666a5141ef2a570ef4db21b039d6d6958c28b
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-30 21:37:41 +0000

    Code update: minor changes

---
+++ src/main/java/com/backend/liaison_system/users/student/supervision/SupervisionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.backend.liaison_system.users.student.supervision;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SupervisionServiceImpl implements SupervisionService{
+
+}
