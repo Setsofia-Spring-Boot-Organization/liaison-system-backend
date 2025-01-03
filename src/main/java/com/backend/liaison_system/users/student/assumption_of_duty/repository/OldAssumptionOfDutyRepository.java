d19db498cb9408f886e04bd4c7c09f5989b5b9d2
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-03 22:21:56 +0000

    Code update: minor changes

--- src/main/java/com/backend/liaison_system/users/student/assumption_of_duty/repository/OldAssumptionOfDutyRepository.java
+++ src/main/java/com/backend/liaison_system/users/student/assumption_of_duty/repository/OldAssumptionOfDutyRepository.java
@@ -3,4 +3,9 @@ package com.backend.liaison_system.users.student.assumption_of_duty.repository;
 import com.backend.liaison_system.users.student.assumption_of_duty.entities.OldAssumptionOfDuty;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OldAssumptionOfDutyRepository extends CrudRepository<OldAssumptionOfDuty, String> { }
+import java.util.List;
+
+public interface OldAssumptionOfDutyRepository extends CrudRepository<OldAssumptionOfDuty, String> {
+
+    List<OldAssumptionOfDuty> findByAssumptionId(String assumptionId);
+}
