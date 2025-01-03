95fa5a675dcb344ebbe0d57ce074f58957916155
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-03 21:21:32 +0000

    Code update: minor changes

---
+++ src/main/java/com/backend/liaison_system/users/student/assumption_of_duty/repository/OldAssumptionOfDutyRepository.java
@@ -0,0 +1,6 @@
+package com.backend.liaison_system.users.student.assumption_of_duty.repository;
+
+import com.backend.liaison_system.users.student.assumption_of_duty.entities.OldAssumptionOfDuty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OldAssumptionOfDutyRepository extends CrudRepository<OldAssumptionOfDuty, String> { }
